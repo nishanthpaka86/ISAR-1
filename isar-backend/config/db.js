659fef7a0402fe091adbc65b2d798e2d82193f57
nishanthpaka86 <154611112+nishanthpaka86@users.noreply.github.com>
2025-07-05 13:04:02 +0530

    db.js

--- isar-backend/config/db.js
+++ isar-backend/config/db.js
@@ -1,11 +1,12 @@
 
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'ISAR', // replace with your actual MySQL password
-  database: 'isar_db', // replace with your actual DB name
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,     // not 'localhost'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
