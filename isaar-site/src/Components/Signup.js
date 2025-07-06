5eff6731b3568db20726028fabbe58d856881b67
nishanthpaka86 <154611112+nishanthpaka86@users.noreply.github.com>
2025-07-06 13:00:34 +0530

    Update Signup.js

--- isaar-site/src/Components/Signup.js
+++ isaar-site/src/Components/Signup.js
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('https://isar-backend.onrender.com/api/auth/login', {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
