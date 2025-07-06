c7be2922674187e98a75b5276f547d74fb4399b0
nishanthpaka86 <154611112+nishanthpaka86@users.noreply.github.com>
2025-07-06 12:46:14 +0530

    Update Enrollments.js

--- isar-admin/src/components/Enrollments.js
+++ isar-admin/src/components/Enrollments.js
@@ -6,7 +6,7 @@ const Enrollments = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch('${process.env.REACT_APP_API_URL}/api/admin/enrollments')
+    fetch(`${process.env.REACT_APP_API_URL}/api/admin/enrollments`)
       .then(res => res.json())
       .then(data => {
         if (data.success) {
